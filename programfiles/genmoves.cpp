dc612060ee06038b9cfa5e81c73dd6a84c551f38
AT8Cool <leo8gaming@gmail.com>
2026-05-31 10:01:19 +0530

    Add white pawn capture generation

--- programfiles/genmoves.cpp
+++ programfiles/genmoves.cpp
@@ -15,16 +15,38 @@ std::vector<Square> generateWhitePawnMoves(char (&board)[9][9],Square position){
      int forward = position.row -1;
      int doubleForward = position.row - 2;
 
-    
+    //Movements    
     if(board[forward][position.col] == '.'){
     
     moves.push_back({forward,position.col});
-    
+
         if(board[doubleForward][position.col] == '.' && position.row == 6){
              moves.push_back({doubleForward, position.col});
         }
    }
+    // Capturing moves
+
+    // Check left diagonal
+    if(position.col > 1){
+            if(islower(board[forward][position.col-1])){
+            moves.push_back({forward,position.col-1});
+        }
+    }
+   
+
+    //Check right diagonal
+    if(position.col < 8){
+            if(islower(board[forward][position.col+1])){
+            moves.push_back({forward,position.col+1});
+        }
+    }
+    
+
+
+   
 
 
     return moves;
 }
+
+
