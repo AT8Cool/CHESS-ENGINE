627c7f5b7960e6f8c6deb391e2a6e1ef4273a94a
AT8Cool <leo8gaming@gmail.com>
2026-06-05 09:13:53 +0530

    Refactored evaluate() using pieceValue()

--- programfiles/evaluate.cpp
+++ programfiles/evaluate.cpp
@@ -10,32 +10,38 @@ Queen  = 900
 */ 
 
 #include "evaluate.h"
+int pieceValue(char piece){
+switch (piece)
+            {
+            case 'P': return 100;
+            case 'N': return 300;
+            case 'B': return 300;
+            case 'R': return 500;
+            case 'Q': return 900;
+            case 'K': return 20000;
+
+            case 'p': return -100;
+            case 'n': return -300;
+            case 'b': return -300;
+            case 'r': return -500;
+            case 'q': return -900;
+            case 'k': return -20000;
+
+            default: return 0;
+        
+            }
+
+
+
+}
 
 int evaluate(Position &position){
     int score =0;
     for(int row = 0; row<8; row++){
         for(int col =1; col <=8; col++){
-            char piece = position.board[row][col];
+            score += pieceValue(position.board[row][col]);
 
-            switch (piece)
-            {
-            case 'P':score += 100;break;
-            case 'N':score += 300;break;
-            case 'B':score += 300;break;
-            case 'R':score += 500;break;
-            case 'Q':score += 900;break;
-            //FOR TESTING PURPOSE ONLY LATER REMOVE THE BELOW CASE
-            case 'K': score +=20000; break;
             
-            case 'p':score -= 100;break;
-            case 'n':score -= 300;break;
-            case 'b':score -= 300;break;
-            case 'r':score -= 500;break;
-            case 'q':score -= 900;break;
-            //FOR TESTING PURPOSE ONLY LATER REMOVE THE BELOW CASE
-            case 'k': score -=20000; break;
-        
-            }
         }
     }
     return score;
